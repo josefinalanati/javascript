e0f30004b79f02736cb2e60aa42c9525fd6c1c76
Josefina Lanati <lanati.josefina@gmail.com>
2021-03-29 18:32:12 -0300

    ifelse

--- java.js
+++ java.js
@@ -118,9 +118,13 @@ console.log(months)
 var specificmonths = months.slice(4,11);
 console.log(specificmonths);
 
-
-
-
-
-
-
+//** If else */
+//**   a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor 
+//o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” 
+//y sino un alerta con el mensaje “Lower than 0,5” */
+var newnumber = Math.random();
+console.log(newnumber)
+if (newnumber >= 0,5)
+	console.log ("Greater than 0,5");
+else
+	console.log("Lower than 0,5");
