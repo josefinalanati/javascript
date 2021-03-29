cc166e7572b133adb35cb65c839f77bdf51d94ff
Josefina Lanati <lanati.josefina@gmail.com>
2021-03-29 13:19:02 -0300

    2.a

--- java.js
+++ java.js
@@ -23,3 +23,12 @@ apellido = "Lopez";
 var personal = nombre + apellido
 var identidad = nombre.length + apellido.length;
 console.log(personal, identidad);
+
+//****Strings ****/
+//    a) Crear una variable de tipo string con 
+//al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase) */
+var text
+text = "Mi nombre es Pablo Lopez";
+var texttwo
+texttwo = text.toUpperCase(text)
+console.log(texttwo)
