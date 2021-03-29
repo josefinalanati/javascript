06231b0117c95ff06ef357f06041dd04b0563082
Josefina Lanati <lanati.josefina@gmail.com>
2021-03-29 11:27:11 -0300

    1.b

---
+++ java.js
@@ -0,0 +1,9 @@
+console.log ("JavaScript Exercises");
+//****Variables y Operadores*****/
+// a) Crear dos variables numéricas y utilizar el operador  
+//    suma para guardar el valor de la suma de ambos números en una 3er variable */ 
+var vasos, platos;
+vasos = 20;
+platos = 10;
+var cantidadMercaderia = vasos + platos;
+console.log(cantidadMercaderia)
